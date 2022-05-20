b0491d6d83182d1bb089bd3efbc04f76f28f494c
Mario Schuetz <mario.schuetz@leanovate.de>
2022-05-20 08:37:49 +0200

    Extract stream listener and get untruncated tweet

--- lean_bot.py
+++ lean_bot.py
@@ -2,11 +2,12 @@ import tweepy
 import configparser
 from textblob import TextBlob
 import time
+from streamlistener import Listener
 
-# keywords = ["Kanban", "#kanban", "Scrum", "#scrum"]
-# keywords = ["Kanban", "#kanban"]
-keywords = ["Scrum", "#scrum"]
-languages = ["en", "de"]
+# keywords = ['Kanban', '#kanban', 'Scrum', '#scrum']
+# keywords = ['Kanban', '#kanban']
+keywords = ['Scrum', '#scrum']
+languages = ['en', 'de']
 
 config = configparser.ConfigParser()
 config.read('config')
@@ -22,24 +23,11 @@ bot_id = int(api.verify_credentials().id)
 
 try:
     # print(api.verify_credentials())
-    print(f"Login successful (id: {bot_id})")
+    print(f'Login successful (id: {bot_id})')
 except tweepy.TweepyException as e:
     print(e)
 except Exception as e:
     print(e)
-
-
-class Listener(tweepy.Stream):
-    def on_status(self, tweet):
-
-        if not tweet.truncated:
-            print(f"{tweet.user.screen_name}: {tweet.text}")
-        else:
-            print(f"{tweet.user.screen_name}: {tweet.extended_tweet['full']}")q
-
-        # if len(self.tweets) == self.limit:
-        #     self.disconnect()
-
 
 stream_listener = Listener(api_key, api_key_secret, access_token, access_token_secret)
 
--- streamlistener.py
+++ streamlistener.py
@@ -1,29 +1,44 @@
 import tweepy
 
+#
+# class Listener(tweepy.Stream):
+#     def __init__(self, api):
+#         self.api = api
+#         self.me = api.verify_credentials()
+#
+#     def on_status(self, tweet):
+#         if tweet.in_reply_to_status_id is not None or tweet.use.id == self.me.id:
+#             return
+#
+#         if not tweet.retweeted:
+#             try:
+#                 # tweet.retweet()
+#                 print(f"{tweet.author.screen_name} - {tweet.text}")
+#                 print(f"Tweet ({tweet.in_reply_to_status_id}) retweeted successfully")
+#             except Exception as e:
+#                 print(e)
+#
+#         if not tweet.favorited:
+#             try:
+#                 # tweet.favorite()
+#                 print(f"Tweet ({tweet.in_reply_to_status_id}) favorited successfully")
+#             except Exception as e:
+#                 print(e)
+#
+#     def on_error(self, status):
+#         print(f"Error while retweeting: {status}")
+
 
 class Listener(tweepy.Stream):
-    def __init__(self, api):
-        self.api = api
-        self.me = api.verify_credentials()
-
     def on_status(self, tweet):
-        if tweet.in_reply_to_status_id is not None or tweet.use.id == self.me.id:
-            return
 
-        if not tweet.retweeted:
-            try:
-                # tweet.retweet()
-                print(f"{tweet.author.screen_name} - {tweet.text}")
-                print(f"Tweet ({tweet.in_reply_to_status_id}) retweeted successfully")
-            except Exception as e:
-                print(e)
+        if not tweet.truncated:
+            print(f'{tweet.user.screen_name}: {tweet.text}')
+        else:
+            print(f'{tweet.user.screen_name}: {tweet.extended_tweet["full_text"]}')
 
-        if not tweet.favorited:
-            try:
-                # tweet.favorite()
-                print(f"Tweet ({tweet.in_reply_to_status_id}) favorited successfully")
-            except Exception as e:
-                print(e)
+        # if len(self.tweets) == self.limit:
+        #     self.disconnect()
 
     def on_error(self, status):
         print(f"Error while retweeting: {status}")
